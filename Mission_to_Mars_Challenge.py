f129018fbb33d9ca8444c668d6a0b73da64e623e
shireenkahlon <75495212+shireenkahlon@users.noreply.github.com>
2021-03-13 21:44:26 -0600

    Base challenge code

--- Mission_to_Mars_Challenge.py
+++ Mission_to_Mars_Challenge.py
@@ -1,5 +1,5 @@
 
-#import Splinter, BeautifulSoup, Pandas, webdriver, ChromeDriverManager, time, and random
+# import Splinter, BeautifulSoup, Pandas, webdriver, ChromeDriverManager, time, and random
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
@@ -7,7 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 import random
 
-#Set the executable path
+# Set the executable path
 executable_path={'executable_path':'/usr/local/bin/chromedriver'}
 browser=Browser('chrome',**executable_path,headless=False)
 
@@ -15,24 +15,24 @@ browser=Browser('chrome',**executable_path,headless=False)
 url='https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
 browser.visit(url)
 
-#Delay for loading the page
+# Delay for loading the page
 browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
-#set up html parser
+# set up html parser
 html=browser.html
 news_soup=soup(html,'html.parser')
 slide_elem = news_soup.select_one('ul.item_list li.slide')
 # print(slide_elem)
 
-#scrape the title
+# Scrape the title
 news_title=slide_elem.find('div',class_='content_title').get_text()
 news_p=slide_elem.find('div',class_='article_teaser_body').get_text()
 print(news_p)
 
 
-# ### Featured Images
+### Featured Images
 
-#visit url
+# Visit url
 url='https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
 browser.visit(url)
 
@@ -49,36 +49,36 @@ img_url_rel=img_soup.find('img',class_='fancybox-image').get('src')
 img_url=f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
 img_url
 
-#Scrape the table data from space-facts.com
+# Scrape the table data from space-facts.com
 df=pd.read_html('https://space-facts.com/mars/')[0]
 df.columns=['description','value']
 df.set_index('description',inplace=True)
 df
 df.to_html
 
-#quit browser
+# Quit browser
 browser.quit()
 
-# # D1: Scrape High-Resolution Mars’ Hemisphere Images and Titles
+### D1: Scrape High-Resolution Mars’ Hemisphere Images and Titles
 
-# ### Hemispheres
+### Hemispheres
 
-# 1. Use browser to visit the URL 
+# Use browser to visit the URL 
 url = 'https://data-class-mars-hemispheres.s3.amazonaws.com/Mars_Hemispheres/index.html'
 
 browser.visit(url)
 html=browser.html
 mars_soup=soup(html,'html.parser')
 
-#Create list for hemisphere_image_urls
+# Create list for hemisphere_image_urls
 hemisphere_image_urls = []
 links = browser.find_by_css('a.product-item img')
 
-#loop through site to scrape title and image
+# Loop through site to scrape title and image
 for i in range(len(links)):
     hemisphere = {}
     browser.find_by_css('a.product-item img')[i].click()
-    # find the Sample image - anchor tag and extract href
+    # Find the Sample image - anchor tag and extract href
     sample_elem = browser.links.find_by_text('Sample').first
     hemisphere['img_url'] = sample_elem['href']
     # Get Hemisphere title
