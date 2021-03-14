bccb04930ad1e39af7978e63a5a5fc556862d323
shireenkahlon <75495212+shireenkahlon@users.noreply.github.com>
2021-03-13 21:46:31 -0600

    Functions to add scraped data to a Flask site

--- apps.py
+++ apps.py
@@ -1,24 +1,22 @@
-#import dependencies
+# import flask.Flask, flask.render_template, flask.redirect, flask_pymongo.PyMongo, scrape
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo, MongoClient
+from flask_pymongo import PyMongo
 import scrape
 
-# #Set up Flask
+#Set up Flask
 app=Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = 'mongodb://localhost:27017/mars_app'
 mongo = PyMongo(app)
-# client = MongoClient('mongodb://localhost:27017/mars_app')
-# db=mongo.mars_app
 
-# define the route for the HTML page
+# Define the route for the HTML page
 @app.route("/")
 def index():
     mars=mongo.db.mars.find_one()
     return render_template('index.html', mars=mars)
 
-#define the scraped data route
+# Define the scraped data route
 @app.route("/scrape")
 def scraper():
     mars=mongo.db.mars
